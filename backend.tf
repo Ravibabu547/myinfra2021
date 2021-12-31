e53caa93056ba4b22adb7390fd882a8792299a04
Ravibabu547 <46025569+Ravibabu547@users.noreply.github.com>
2021-12-31 10:45:59 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "myazurebucket123"
+        bucket = "my-tf-state-s3"
         key = "terraform.tfstate"
         region = "us-east-1"
 		dynamodb_table="my-dynamodb-table"
